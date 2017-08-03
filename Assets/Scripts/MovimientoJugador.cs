c4f06faef08ef80bec7061ad4725f08eaa2363ca
Manuel Monzonis Franch <al286546@uji.es>
2017-08-03 13:15:28 +0200

    Correr y giro 180º
    implementada la función de correr y la de girar 180º, el giro de 180º
    tiene un bug que a veces bloquea el giro normal

--- Assets/Scripts/MovimientoJugador.cs
+++ Assets/Scripts/MovimientoJugador.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class MovimientoJugador : MonoBehaviour {
     private float valorMovimiento;
     private float valorRotación;
-    public float velocidad = 6.5f;
-    public float velocidadRotacion = 100f;
+    public float velocidad;
+    private float velocidadCorrer;
+    public float velocidadRotacion;
+    private float velocidadMovimiento;
+    private float rotaciónInicial;
+    private float rotaciónFinal;
     private Rigidbody j_rigidbody;
+    private bool rotando = false;
+    private bool bloquearMovimiento = false;
+    private Quaternion rotación;
+    private Quaternion final;
 
     private void Awake()
     {
@@ -16,8 +24,9 @@ public class MovimientoJugador : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
-	}
+        velocidadCorrer = velocidad * 1.5f;
+        velocidadMovimiento = velocidad;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -34,22 +43,63 @@ public class MovimientoJugador : MonoBehaviour {
 
     private void FixedUpdate()
     {
-        Mover();
+        if (!bloquearMovimiento)
+        {
+            Mover();
+        }
         Girar();
+        Giro180();
+        Apuntar();
     }
 
     private void Mover()
     {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            velocidadMovimiento = velocidadCorrer;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            velocidadMovimiento = velocidad;
+        }
         if(valorMovimiento < 0f)
         {
-            Vector3 movimiento = transform.forward * valorMovimiento/1.5f * velocidad * Time.deltaTime;
+            Vector3 movimiento = transform.forward * valorMovimiento/1.5f * velocidadMovimiento * Time.deltaTime;
             j_rigidbody.MovePosition(j_rigidbody.position + movimiento);
         }
         else
         {
-            Vector3 movimiento = transform.forward * valorMovimiento * velocidad * Time.deltaTime;
+            Vector3 movimiento = transform.forward * valorMovimiento * velocidadMovimiento * Time.deltaTime;
             j_rigidbody.MovePosition(j_rigidbody.position + movimiento);
         }      
+    }
+
+    private void Giro180()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space) && valorMovimiento <= 0f)
+        {
+            rotando = true;
+            rotaciónInicial = transform.rotation.y;
+            rotaciónFinal = j_rigidbody.rotation.y + 180f;
+            //bloquearMovimiento = true;
+            final = Quaternion.LookRotation(-transform.forward, Vector3.up);
+        }
+        if (rotando)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, final, 5f * Time.deltaTime);
+            
+        }
+        if (transform.rotation.y == final.y)
+        {
+            rotando = false;
+            //bloquearMovimiento = false;
+        }
+    }
+
+    private void Apuntar()
+    {
+
     }
 
     private void Girar()

e0504b17d0b4d0fd7914cf5da7b9576b2d617b49
Nelocanelo <nelocanelo@gmail.com>
2017-08-10 13:42:28 +0200

    Primer zombi
    Añadido un escenario temporal, y un primer zombi que busca al jugador

---
+++ Assets/Scripts/IAZombi.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IAZombi : MonoBehaviour {
+
+    private NavMeshAgent agente;
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        agente = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agente.destination = player.transform.position;
+	}
+}

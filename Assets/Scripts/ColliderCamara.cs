9957bc454ef19e1e0ef9ac08053ce007cfb04e0b
Nelocanelo <nelocanelo@gmail.com>
2017-08-13 14:04:20 +0200

    Código de cámaras básico (no funciona)
    Algo falla, y creo que es la crear la lista de cámaras.

---
+++ Assets/Scripts/ColliderCamara.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderCamara : MonoBehaviour {
+
+    private Camera miCamara;
+
+    private void Start()
+    {
+        miCamara = gameObject.GetComponentInChildren<Camera>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log(miCamara.name);
+            ControlCamara.CambiarCamara(miCamara);
+        }
+    }
+}
---
+++ Assets/Scripts/ControlCamara.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCamara : MonoBehaviour {
+
+    private static List<Camera> camaras;
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] auxcamaras = GameObject.FindGameObjectsWithTag("MainCamera");
+
+        for (int i = 0; i < auxcamaras.Length; i++)
+        {
+            camaras.Add(auxcamaras[i].GetComponent<Camera>());
+        }
+	}
+	
+	public static void CambiarCamara(Camera camara)
+    {
+        foreach (Camera c in camaras)
+        {
+            c.enabled = false;
+        }
+        camara.enabled = true;
+    }
+}
